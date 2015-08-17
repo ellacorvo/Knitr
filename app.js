f24c6251d4c459134b92e8a50c0072cd5c13eb8d
Lauren Shugrue <ellacorvo@gmail.com>
2015-08-17 16:38:44 -0600

    routing

--- app.js
+++ app.js
@@ -12,6 +12,17 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', indexController.index);
+app.post('/yarns', function(req, res){
+	console.log(req.body);
+	var newYarn = new Yarn({ 
+		name : req.body.yards, 
+		color : req.body.color
+	});
+	newYarn.save(function(){
+		res.render('yarns');
+	})
+	console.log(newYarn);
+});
 
 var server = app.listen(5395, function() {
 	console.log('Express server listening on port ' + server.address().port);
--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,43 @@
+var Yarn = require('../models/yarn');
+// var Project = require('../models/project');
+
 var indexController = {
 	index: function(req, res) {
 		res.render('index');
-	}
+	},
+	getYarn: function(req, res) {
+        Yarn.find({}, function(err, yarn){
+            res.send(yarn)
+        });	
+	},
+    create : function(req, res){
+        var yarn = new yarn(req.body);
+        yarn.save(function(err, yarn){
+            res.send(yarn)
+        });
+    }
 };
 
 module.exports = indexController;
+
+
+// var apiController = {
+//     get: function(req, res){
+//         console.log(req.user)
+//         res.send(req.user)
+//     },
+//     getArtPieces : function(req, res){
+
+//         yarn.find({}, function(err, artpieces){
+//             res.send(artpieces)
+//         });
+//     },
+//     create : function(req, res){
+//         var artpiece = new artPiece(req.body);
+//         artpiece.save(function(err, artpiece){
+//             res.send(artpiece)
+//         });
+//     }
+// }
+
+// module.exports = apiController;
