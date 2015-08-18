d6f7dff7a1f85fe9adf382c3edce7d2b1861a79b
Lauren Shugrue <ellacorvo@gmail.com>
2015-08-18 11:10:23 -0600

    updated https

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', indexController.index);
 
-var port = process.env.PORT || 3000;
+var server = app.listen(process.env.PORT || 3000);
 
-var server = app.listen(port, function() {
-	console.log('Express server listening on port ' + server.address().port);
-});
+// var server = app.listen(port, function() {
+// 	console.log('Express server listening on port ' + server.address().port);
+// });
