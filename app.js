ec19d13dec66395ebe4ec48b49855fcbc6eb069e
Lauren Shugrue <ellacorvo@gmail.com>
2015-08-18 11:04:31 -0600

    updated port

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', indexController.index);
 
-var server = app.listen(3000, function() {
+var port = process.env.PORT || 3000;
+
+var server = app.listen(port, function() {
 	console.log('Express server listening on port ' + server.address().port);
 });
